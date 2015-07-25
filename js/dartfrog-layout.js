887bb760326de5bf4ad47b9d1283810781aa5b26
Matt Eason <github@matteason.co.uk>
2015-07-25 11:31:24 +0100

    Add long-running query Tetris game

--- js/dartfrog-layout.js
+++ js/dartfrog-layout.js
@@ -12,6 +12,8 @@ var schemaSelectList = '';
 var currentTableMetadata;
 var currentTableData;
 var currentFilePath;
+var gameTimer;
+var gameTick;
 
 $(function () {
   // Set up tabs
@@ -29,11 +31,19 @@ $(function () {
     }
   });
 
+  var toolbar = '<div class="toolbar">'
+    +'<button onclick="javascript:runStatementUnderCursor();dfl.startGameTimer();" id="run" class="icon-play3">Run</button>'
+    +'<button onclick="javascript:runExplianPlanUnderCurrentCursor();" id="explain" class="icon-truck">Plan</button>'
+    +'<button onclick="javascript:dfl.chooseFile(\'#fileDialog\');" id="open" class="icon-folder-open">Open</button>'
+    +'<button onclick="javascript:dfl.saveFile(null);" id="save" class="icon-floppy-disk">Save</button>'
+    +'<button onclick="javascript:dfl.saveAs();" id="saveAs">Save As...</button><span id="filePath"></span>'
+    +'</div>'; 
+
   //Set up editor view
   $('#editor').w2layout({
     name: 'editor',
     panels: [
-      { type: 'main', content: '<div class="toolbar"><button onclick="javascript:runStatementUnderCursor();" id="run" class="icon-play3">Run</button><button onclick="javascript:dfl.chooseFile(\'#fileDialog\');" id="open" class="icon-folder-open">Open</button><button onclick="javascript:dfl.saveFile(null);" id="save" class="icon-floppy-disk">Save</button><button onclick="javascript:dfl.saveAs();" id="saveAs">Save As...</button></div><!--button onclick="javascript:getTableMetaData();">tablemetadata</button--><textarea id="code">select * from dual</textarea>',  resizable: true, size: 50},
+      { type: 'main', content: toolbar+'<textarea id="code">select * from dual</textarea>',  resizable: true, size: 50},
       { type: 'preview', content: 'preview', resizable: true, size: 500}
     ]
   });
@@ -182,6 +192,8 @@ dfl = {
     var data = results;
     var keys;
 
+    dfl.stopGame();
+
     if(results.length > 0){
       keys = Object.keys(results[0]);
 
@@ -234,6 +246,8 @@ dfl = {
       chooser.unbind('change');
       chooser.change(function(evt) {
         currentFilePath = $(this).val();
+        $('#filePath').text(currentFilePath);
+
         fs.readFile($(this).val(),null,function(err,data){
           if(err){
             alert(err);
@@ -265,6 +279,9 @@ dfl = {
       var chooser = $('#saveAs');
       chooser.unbind('change');
       chooser.change(function(evt) {
+        currentFilePath = $(this).val();
+        $('#filePath').text(currentFilePath);
+
         fs.writeFile($(this).val(), editor.getValue(), function(err) {
             if(err) {
                 alert("error");
@@ -275,6 +292,56 @@ dfl = {
       });
 
       chooser.trigger('click');       
+    },
+
+    startGameTimer: function() {
+      gameTimer = setTimeout(dfl.startGame, 3000);
+    },
+
+    startGame: function() {
+      $('#game-popup').show(250);
+      var fs = "1111:01|01|01|01*011|110:010|011|001*110|011:001|011|010*111|010:01|11|01:010|111:10|11|10*11|11*010|010|011:111|100:11|01|01:001|111*01|01|11:100|111:11|10|10:111|001", now = [3,0], pos = [4,0];
+      var gP = function(x,y) { return document.querySelector('[data-y="'+y+'"] [data-x="'+x+'"]'); };
+      var draw = function(ch, cls) {
+          var f = fs.split('*')[now[0]].split(':')[now[1]].split('|').map(function(a){return a.split('')});
+          for(var y=0; y<f.length; y++)
+              for(var x=0; x<f[y].length; x++)
+                  if(f[y][x]=='1') {
+                      if(x+pos[0]+ch[0]>9||x+pos[0]+ch[0]<0||y+pos[1]+ch[1]>19||gP(x+pos[0]+ch[0],y+pos[1]+ch[1]).classList.contains('on')) return false;
+                      gP(x+pos[0]+ch[0], y+pos[1]+ch[1]).classList.add(cls!==undefined?cls:'now');
+                  }
+          pos = [pos[0]+ch[0], pos[1]+ch[1]];
+      }
+      var deDraw = function(){ if(document.querySelectorAll('.now').length>0) deDraw(document.querySelector('.now').classList.remove('now')); }
+      var check = function(){
+        for(var i=0; i<20; i++)
+          if(document.querySelectorAll('[data-y="'+i+'"] .brick.on').length == 10) 
+            return check(roll(i), document.querySelector('#result').innerHTML=Math.floor(document.querySelector('#result').innerHTML)+10);
+      };
+      var roll = function(ln){ if(false !== (document.querySelector('[data-y="'+ln+'"]').innerHTML = document.querySelector('[data-y="'+(ln-1)+'"]').innerHTML) && ln>1) roll(ln-1); };
+      window.addEventListener('keydown', kdf = function(e){
+          if(e.keyCode==38&&false!==(now[1]=((prv=now[1])+1)%fs.split('*')[now[0]].split(':').length) && false===draw([0,0], undefined, deDraw())) draw([0,0],undefined, deDraw(), now=[now[0],prv]);
+          if((e.keyCode==39||e.keyCode==37)&&false===draw([e.keyCode==39?1:-1,0],undefined,deDraw())) draw([0,0],undefined,deDraw());
+          if(e.keyCode == 40)
+              if(false === draw([0,1], undefined, deDraw())) {
+                  if(draw([0,0], 'on', deDraw())||true) check();
+                  if(false === draw([0,0], undefined, now = [Math.floor(Math.random()*fs.split('*').length),0], pos = [4,0])) { 
+              toV=-1; 
+              alert('Your score: '+document.querySelector('#result').innerHTML); 
+            }
+              }
+      });
+      toF = function() {
+          kdf({keyCode:40});
+          gameTick = setTimeout(function(){if(toV>=0)toF();}, toV=toV>0?toV-0.5:toV);
+      }
+      toF(toV = 500);
+    },
+
+    stopGame: function() {
+      $('#game-popup').hide(500);
+      clearTimeout(gameTimer);
+      clearTimeout(gameTick);
     }
 };
 
